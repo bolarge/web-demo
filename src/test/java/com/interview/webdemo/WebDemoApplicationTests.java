93cfd6f9f1c800a6dc41cb9ea2e2d5fbd3e50615
Bolaji <bolaji.salau@gmail.com>
2023-09-26 17:26:18 +0100

    setup commit

---
+++ src/test/java/com/interview/webdemo/WebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.webdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
